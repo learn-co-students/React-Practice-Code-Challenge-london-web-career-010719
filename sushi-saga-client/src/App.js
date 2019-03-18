4adec4357be9bba1e1a25aa437f8d4fd5317e1ae
kkoomin <minha.koo.9@gmail.com>
2019-03-18 16:24:18 +0000

    Basic things Done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,54 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushiList: [],
+    selectedSushiList: [],
+    eatenSushiList: [],
+    counter: 1,
+    remainedMoney: 100
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => this.setState({
+      ...this.state,
+      sushiList: data,
+      selectedSushiList: data.slice(0, 4)
+    }))
+  }
+
+  eatSushi = (sushi) => {
+    if (this.state.remainedMoney > sushi.price && !this.state.eatenSushiList.includes(sushi)) {
+      return this.setState({
+        ...this.state,
+        eatenSushiList: [...this.state.eatenSushiList, sushi],
+        remainedMoney: this.state.remainedMoney - sushi.price
+      })
+    } 
+  }
+
+  selectSushi = () => {
+    let n = this.state.counter
+    this.setState({
+      ...this.state,
+      selectedSushiList: this.state.sushiList.slice(4*n, 4*(n+1)),
+      counter: this.state.counter + 1
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer 
+          sushiList={this.state.sushiList} 
+          selectedSushiList={this.state.selectedSushiList} 
+          eatenSushiList={this.state.eatenSushiList} 
+          eatSushi={this.eatSushi}
+          selectSushi={this.selectSushi}/>
+        <Table 
+          eatenSushiList={this.state.eatenSushiList} remainedMoney={this.state.remainedMoney}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,19 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 const Sushi = (props) => {
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={() => props.eatSushi(props.info)}>
         { 
           /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
+          props.eatenSushiList.includes(props.info) ?
+          null :
+          <img src={props.info.img_url} width="100%" alt={props.info.name}/>
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {props.info.name} - ${props.info.price}
       </h4>
     </div>
   )
