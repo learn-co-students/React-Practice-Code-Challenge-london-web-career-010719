0fd69ed160efed71a940a8b509cb12a5042c41eb
studioshane <studioshane@gmail.com>
2019-03-19 12:59:23 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,66 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const URL = "http://localhost:3000/sushis";
 
 class App extends Component {
+  state = {
+    sushis: {},
+    page: 0,
+    balance: 100,
+    consumedSushis: []
+  };
+
+  getSushis = () => {
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(sushis => {
+        this.setState({ sushis });
+      });
+  };
+
+  componentDidMount() {
+    this.getSushis();
+  }
+
+  getSushiRange = (sushiArray, number) => {
+    return [...sushiArray].slice(number * 4, number * 4 + 4);
+  };
+
+  increasePage = () => {
+    if (this.state.page === 24) {
+      this.setState({ page: 0 });
+    } else {
+      this.setState({ page: this.state.page + 1 });
+    }
+  };
+
+  eatSushi = sushi => {
+    if (this.state.balance < sushi.price) {
+      alert("No sushi for you, scrub!");
+    } else {
+      this.setState({
+        consumedSushis: [...this.state.consumedSushis, sushi.id]
+      });
+      this.setState({ balance: this.state.balance - sushi.price });
+    }
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          handleClick={this.eatSushi}
+          consumedSushis={this.state.consumedSushis}
+          sushis={this.getSushiRange(this.state.sushis, this.state.page)}
+          onMoreSushiClick={this.increasePage}
+        />
+        <Table
+          consumedSushis={this.state.consumedSushis}
+          balance={this.state.balance}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,23 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Fragment } from "react";
+import MoreButton from "../components/MoreButton";
+import Sushi from "../components/Sushi";
 
-const SushiContainer = (props) => {
+const SushiContainer = props => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {props.sushis.map(sushi => (
+          <Sushi
+            consumedSushis={props.consumedSushis}
+            handleClick={props.handleClick}
+            key={sushi.id}
+            sushi={sushi}
+          />
+        ))}
+        <MoreButton handleClick={props.onMoreSushiClick} />
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default SushiContainer
+export default SushiContainer;
