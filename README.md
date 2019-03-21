5209bda1b9bb705a646ab26a3aee0878a7015a44
YannickHoegerle <yannick.hoegerle@gmail.com>
2019-03-21 09:11:09 +0000

    Done.

--- README.md
+++ README.md
@@ -38,7 +38,12 @@ Inspectors will be coming by to check that our patented Sushi Saga conveyor belt
 2. Only 4 sushi are rendered at a time
 3. Clicking the "More Sushi!" button shows the next set of 4 sushi in the list. For this assignment, you don't have to be concerned about what happens when you reach the end of the sushi list.
 4. Clicking a sushi on a plate will eat the sushi, causing it to be removed from its plate and an empty plate to appear on the table.
+
+xxxxxx
+
+
 5. We need to make money! Whenever a sushi is eaten, customers should be automatically charged! Based on a budget decided by you, the developer, the amount of money remaining should go down by the cost of the sushi that was eaten. There is a spot to display this number in the `Table` component
+
 6. No free meals! Customers cannot eat any sushi that exceeds the amount of money remaining in their balance
 
 ### Bonus!
--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,54 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    allSushis: [],
+    eatenSushis: [],
+    budget: Math.floor(Math.random() * 100) + 50,
+    insufficientFunds: false
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushis => this.setState({allSushis: sushis}))
+  }
+
+  eatSushi = (sushi) => {
+    if (this.state.budget < sushi.price) {
+      this.setState({insufficientFunds: true})
+      alert('Sorry, insufficient funds.')
+    } else {
+      if (!this.state.eatenSushis.includes(sushi.id)) {
+        this.setState({eatenSushis: [...this.state.eatenSushis, sushi.id]})
+        this.setState({budget: this.state.budget - sushi.price})
+      }
+    }
+  }
+
+  // eatSushi = sushi => {
+  //   if (this.state.eatenSushis.includes(sushi)) return;
+  //   if (this.state.budget < sushi.price) return this.setState({ insufficientFunds: true })
+  //
+  //   this.setState({
+  //     eatenSushis: [...this.state.eatenSushis, sushi.id],
+  //     budget: this.state.budget - sushi.price
+  //   })
+  // }
+
   render() {
+
+    const sushis = this.state.allSushis.map(sushi => {
+      return {
+        ...sushi,
+        isEaten: this.state.eatenSushis.includes(sushi.id)
+      }
+    })
+
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer allSushis={sushis} eatSushi={this.eatSushi} />
+        <Table budget={this.state.budget} eatenSushis={this.state.eatenSushis}/>
       </div>
     );
   }
